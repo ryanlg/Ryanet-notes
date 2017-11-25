a0d5cc67263af62df5fc55a95bb2970cb931b1c6
Ryan Liang <xmomowan@gmail.com>
2017-11-25 12:15:37 -0600

    cleanup and change mavan complier settings to 1.9

--- src/main/java/io/ryanliang/ryanet/RyanetInitializer.java
+++ src/main/java/io/ryanliang/ryanet/RyanetInitializer.java
@@ -1,13 +1,7 @@
 package io.ryanliang.ryanet;
 
 import io.ryanliang.ryanet.config.WebConfig;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 
 public class RyanetInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
