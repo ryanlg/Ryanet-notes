7d339b8c862e93b3f06b719bdfe40549f08e389a
Ryan Liang <xmomowan@gmail.com>
2018-01-08 23:58:57 +0800

    npm init'd

--- backend/src/main/java/io/ryanliang/ryanet/controller/api/NoteController.java
+++ backend/src/main/java/io/ryanliang/ryanet/controller/api/NoteController.java
@@ -1,16 +1,20 @@
-package io.ryanliang.ryanet.controller;
+package io.ryanliang.ryanet.controller.api;
 
 import io.ryanliang.ryanet.model.Note;
 import io.ryanliang.ryanet.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
-@RequestMapping("/note")
+@RequestMapping("/api/note")
 public class NoteController {
 
     private NoteService noteService;
@@ -21,13 +25,18 @@ public class NoteController {
         this.noteService = noteService;
     }
 
-    @RequestMapping(value = "{id}", method = RequestMethod.GET)
-    public String getNoteWithID(@PathVariable Integer id, ModelMap modelMap) {
+    @GetMapping(value = "/{id}", produces = "application/json; charset=UTF-8")
+    @ResponseBody
+    public Note getNoteWithID(@PathVariable Integer id) {
 
-        Note requestedNote = noteService.findOneByID(id);
-        modelMap.addAttribute("requestedNote", requestedNote);
+        return noteService.findOneByID(id);
+    }
 
-        return "";
+    @GetMapping(value = "list", produces = "application/json; charset=UTF-8")
+    @ResponseBody
+    public List getAllNotes(){
+
+        return noteService.findAll();
     }
 }
 
