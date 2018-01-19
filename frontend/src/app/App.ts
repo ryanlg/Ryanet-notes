d68f93595f01b03bddbf5dabfd65e07ef309a62f
Ryan Liang <xmomowan@gmail.com>
2018-01-19 21:53:54 +0800

    v-for working

--- frontend/src/app/App.ts
+++ frontend/src/app/App.ts
@@ -2,9 +2,11 @@ import axios from 'axios';
 import Vue from 'vue';
 
 export default Vue.extend({
-    data: () => {
+    data() {
+
         return {
-            note: 'Fetching data, or something is wrong' 
+
+            notes: 'Fetching data, or something is wrong',
         }
     },
 
@@ -13,8 +15,14 @@ export default Vue.extend({
         getNote() {
 
             axios.get("http://localhost:8080/Ryanet/api/v1/note/list").then( (response) => {
-                this.note = response.data;
+
+                this.notes = response.data;
             });
+        },
+
+        getPathToSpecificNote(id: String) {
+
+            return "http://localhost:8080/Ryanet/api/v1/note/" + id;
         }
     },
     
