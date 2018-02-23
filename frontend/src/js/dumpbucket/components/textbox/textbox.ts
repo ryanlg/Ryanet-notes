b826a5528bdd1c22b6f269d35eec051ebbc2a2a8
Ryan Liang <xmomowan@gmail.com>
2018-02-23 11:13:00 -0600

    add save to local option, move textbox content to vuex

--- frontend/src/js/dumpbucket/components/textbox/textbox.ts
+++ frontend/src/js/dumpbucket/components/textbox/textbox.ts
@@ -1,17 +1,21 @@
 export default {
+    computed: {
+        content: {
+            get() {
+                return this.$store.getters.dumpbucket_getContent;
+            },
 
-    data() {
-        
-        return {
-            input: '',
-        };
+            set(value: string) {
+                this.$store.dispatch('dumpbucket_setContent', value);
+            },
+        },
     },
 
     methods: {
 
         dump(options: any) {
 
-            this.input = '';
+            this.$store.dispatch('dumpbucket_setContent', '');
         },
     },
 };
--- frontend/src/js/dumpbucket/main.ts
+++ frontend/src/js/dumpbucket/main.ts
@@ -1,10 +1,12 @@
 import { default as textbox } from './components/textbox';
 import { default as submitButton } from './components/submitbutton';
+import { default as options } from './components/options';
 
 export default {
 
     components: {
         textbox,
+        options,
         'submit-button': submitButton,
     },
 
